3ec9571bdd1e78908c176a36b45357dca6dcd92c
testruns-max <testrunsonbit@gmail.com>
2025-10-11 12:31:02 +0200

    Update settings.js for Tone

--- settings.js
+++ settings.js
@@ -1,8 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "SUBZERO~e69de29bb2d1d6434b8b29ae775ad8c2e48c5391",
+  OWNER_NUMBER: "+263788345144", 
+  PREFIX: ".",
+  CDN: "https://mrfrankk-cdn.hf.space"
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
